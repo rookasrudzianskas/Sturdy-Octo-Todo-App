973e88ca379cce4319d8506f9a9049ab9de4c7b6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-28 11:45:15 +0300

    Rokas

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,11 +1,50 @@
 <template>
-  <q-page class="flex flex-center">
-    <p>Todo Page</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'GO to shop',
+          completed: false,
+        },
 
+        {
+          id: 2,
+          name: 'Get to bananas',
+          completed: false,
+        },
+
+        {
+          id: 3,
+          name: 'Go to home',
+          completed: false,
+        },
+      ]
+    }
+  }
 }
 </script>
