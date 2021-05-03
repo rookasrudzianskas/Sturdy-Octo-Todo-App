f5e042adee70a405a58584e7e24490f17a448c50
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 12:46:30 +0300

    Rokas

--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,15 +1,31 @@
 <template>
   <q-page padding>
-		<p>Settings page</p>
+		<q-list bordered padding>
+      <q-item-label header>Settings</q-item-label>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Show 12 hours time format</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-toggle v-model="show12HourTimeFormat" color="blue" />
+
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 	export default {
-
+	  data() {
+	    return {
+        show12HourTimeFormat: false
+      }
+    }
 	}
 </script>
 
 <style>
-	
+
 </style>
---
+++ src/store/store-settings.js
@@ -0,0 +1,28 @@
+
+// state to show all the task objects, as the objects
+const state = {
+  settings: {
+    show12HourTimeFormat: false
+  }
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {
+
+}
+
+export default {
+	namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+
+}
