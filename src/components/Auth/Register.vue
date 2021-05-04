e5ea9d1bd1d217492082c6fd1ddac52e45f477ba
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 11:15:01 +0300

    Rokas

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="">
+  <form action="" @submit.prevent="submitForm">
     <div class="row q-mb-md">
   <q-banner class="bg-grey-3 col">
     <template v-slot:avatar>
@@ -12,13 +12,19 @@
 
     <div class="row q-mb-md">
 
-      <q-input outlined v-model="formData.email" class="col" label="Email" stack-labels/>
+      <q-input outlined v-model="formData.email" class="col" label="Email" stack-labels :rules="[ val => isValidEmailAddress(val) || 'Please use the valid email address']" lazy-rules/>
 
     </div>
 
     <div class="row q-mb-md">
 
-      <q-input outlined v-model="formData.password" class="col" type="password" label="Password" stack-labels/>
+      <q-input outlined v-model="formData.password" class="col" type="password" label="Password" stack-labels :rules="[ val => val.length >= 6 || 'Please use at least 6 characters']" lazy-rules/>
+
+    </div>
+
+    <div class="row">
+      <q-space />
+      <q-btn type="submit" color="primary" label="Register" />
 
     </div>
 
@@ -33,6 +39,15 @@ export default {
         email: '',
         password: '',
       }
+    }
+  },
+  methods: {
+    isValidEmailAddress(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
+    submitForm() {
+
     }
   }
 }
