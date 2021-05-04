db3912db06921e5b22a04531ea26b581c75ba861
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 11:49:14 +0300

    Rokas

---
+++ src/boot/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app"
+import "firebase/auth"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmFbBsmtUpBiq-tGZvlU1L5csEVoSbCUE",
+  authDomain: "awesome-todo-94cbf.firebaseapp.com",
+  databaseURL: "https://awesome-todo-94cbf-default-rtdb.firebaseio.com",
+  projectId: "awesome-todo-94cbf",
+  storageBucket: "awesome-todo-94cbf.appspot.com",
+  messagingSenderId: "404145853498",
+  appId: "1:404145853498:web:a33925ded629f1371e1103",
+  measurementId: "G-58G4ZYCQNL"
+}
+
+let firebaseApp = firebase.initializeApp(firebaseConfig)
+
+let firebaseAuth = firebaseApp.auth()
+
+
+export { firebaseAuth }
